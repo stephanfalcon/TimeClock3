ae8871510f881381e3f1072265bde47d221aba12
stephan <hoobaloob99@gmail.com>
2020-08-29 13:26:07 -0600

    added privacy policy to /privacy

--- server.js
+++ server.js
@@ -14,4 +14,10 @@ app.get('/ping', function (req, res) {
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});app.listen(port);
+});
+  
+app.get('/privacy', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'privacy.html'));
+});
+
+app.listen(port);
